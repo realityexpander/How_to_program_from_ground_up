eb53a3e4c7256588ba40122a4afdc8e7ae3bd787
Chris Athanas <realityexpander@gmail.com>
2024-02-21 16:09:03 -0600

    improve COP

--- 09-ClassOrientedProgramming.md
+++ 09-ClassOrientedProgramming.md
@@ -211,17 +211,17 @@
     
     data class Person(
        var name: String = "John Doe",
-       var age: Int = 0, // never store age like this, always calculate it from the birthdate.
+       var age: Int = 0,  // never store age like this, always calculate it from the birthdate.
        var height: Double = 0.0,
        var weight: Double = 0.0
-    )
+    ) { 
+       fun printPerson() {
+           println("Name: ${this.name}, Age: ${this.age}, Height: ${this.height}, Weight: ${this.weight}")
+       }
+    }
     
     class PersonManager {
-       companion object { // <-- creates a `static` set of functions that can be accessed without an instance of the `PersonManager` class.
-          fun printPerson(person: Person) {
-             println("Name: ${person.name}, Age: ${person.age}, Height: ${person.height}, Weight: ${person.weight}")
-          }
-    
+       companion object {  // <-- creates a `static` set of functions that can be accessed without an instance of the `PersonManager` class.
           fun changeName(person: Person, newName: String) {
                person.name = newName
           }
@@ -243,20 +243,20 @@
     // Start of Program
     fun main() {
        val person = Person("John Doe", 30, 6.0, 200.0)
-       
-       // Using the `static` methods of the `PersonManager` class to manipulate the data of the `Person` object.
-       PersonManager.changeName(person, "Jane Doe")
-       PersonManager.changeAge(person, 25)
-       PersonManager.changeHeight(person, 5.5)
-       PersonManager.changeWeight(person, 150.0)
-       PersonManager.printPerson(person)
-       
-       // or worse, directly accessing the data: (yes its super fast! but...)
+        
+       // Directly accessing the data, just like in the Procedural & Structured paradigms.
        person.name = "Jane Doe"
        person.age = 25
        person.height = 5.5
        person.weight = 150.0
-       PersonManager.printPerson(person)
+       person.printPerson()   
+     
+       // COP Using the `static` methods of the `PersonManager` class to manipulate the data of the `Person` object.
+       PersonManager.changeName(person, "Jane Doe")
+       PersonManager.changeAge(person, 25)
+       PersonManager.changeHeight(person, 5.5)
+       PersonManager.changeWeight(person, 150.0)
+       person.printPerson()
     }
     
     // Output:
--- src/main/kotlin/usingClassesAsNameSpaces.kt
+++ src/main/kotlin/usingClassesAsNameSpaces.kt
@@ -1,32 +1,49 @@
 // Kotlin Example of Using a Class as a "Name-Space" to Group Together Methods
 // - Using COP style mashed together with Procedural style.
 
+// The `Person` class is used as a "structure" to group together data that is related to a person.
+// It's COP because it has a method that operates on its own object's data.
 data class Person(
 	var name: String = "John Doe",
 	var age: Int = 0,  // never store age like this, always calculate it from the birthdate.
 	var height: Double = 0.0,
 	var weight: Double = 0.0
-)
+) {
+	// This is a method that is part of the `Person` class. This makes it a COP `class` and not a `struct`.
+	fun printPerson() {
+		println("Name: ${this.name}, Age: ${this.age}, Height: ${this.height}, Weight: ${this.weight}")
+	}
+}
+
+// Here's just a bunch of functions that operate on the `Person` class / "structure", like in procedural and structured programming.
+fun printPerson(person: Person) {
+	println("Name: ${person.name}, Age: ${person.age}, Height: ${person.height}, Weight: ${person.weight}")
+}
+fun changeName(person: Person, newName: String) {
+	person.name = newName
+}
+fun changeAge(person: Person, newAge: Int) {
+	person.age = newAge
+}
+fun changeHeight(person: Person, newHeight: Double) {
+	person.height = newHeight
+}
+fun changeWeight(person: Person, newWeight: Double) {
+	person.weight = newWeight
+}
 
 // This class is used as a "name-space" to group together methods that operate on the `Person` class.
 class PersonManager {
 	companion object { // <-- creates a `static` set of functions that can be accessed without an instance of the `PersonManager` class.
-		fun printPerson(person: Person) {
-			println("Name: ${person.name}, Age: ${person.age}, Height: ${person.height}, Weight: ${person.weight}")
-		}
-
 		fun changeName(person: Person, newName: String) {
 			person.name = newName
 		}
-
 		fun changeAge(person: Person, newAge: Int) {
 			person.age = newAge
 		}
-
 		fun changeHeight(person: Person, newHeight: Double) {
 			person.height = newHeight
 		}
-
 		fun changeWeight(person: Person, newWeight: Double) {
 			person.weight = newWeight
 		}
@@ -38,19 +55,15 @@ class PersonManager2 {
 	fun changeName(person: Person, s: String) {
 		person.name = s
 	}
-
 	fun changeAge(person: Person, i: Int) {
 		person.age = i
 	}
-
 	fun changeHeight(person: Person, d: Double) {
 		person.height = d
 	}
-
 	fun changeWeight(person: Person, d: Double) {
 		person.weight = d
 	}
-
 	fun printPerson(person: Person) {
 		println("Name: ${person.name}, Age: ${person.age}, Height: ${person.height}, Weight: ${person.weight}")
 	}
@@ -60,21 +73,28 @@ class PersonManager2 {
 fun main() {
 	val person = Person("John Doe", 30, 6.0, 200.0)
 
+	// Directly accessing the data, like in procedural & structured programming.
+	person.name = "Jane Doe"
+	person.age = 25
+	person.height = 5.5
+	person.weight = 150.0
+	person.printPerson()  // <-- "touch" of COP because a method is being called on the `person` object, not "just" a function.
+
+	// Using functions like in the procedural and structured programming.
+	changeName(person, "Jane Doe")
+	changeAge(person, 25)
+	changeHeight(person, 5.5)
+	changeWeight(person, 150.0)
+	printPerson(person)
+
 	// Using the `static` methods of the `PersonManager` class to manipulate the data of the `Person` object.
 	PersonManager.changeName(person, "Jane Doe")
 	PersonManager.changeAge(person, 25)
 	PersonManager.changeHeight(person, 5.5)
 	PersonManager.changeWeight(person, 150.0)
-	PersonManager.printPerson(person)
+	person.printPerson()
 
-	// or worse, directly accessing the data: (yes its super fast! but...)
-	person.name = "Jane Doe"
-	person.age = 25
-	person.height = 5.5
-	person.weight = 150.0
-	PersonManager.printPerson(person)
-
-	// Another common mistake:
+	// Another common mistake, that is equivalent to above.
 	val pm = PersonManager2()  // <-- Creating an object just to call a method is a waste of memory. Why not just call a function?
 	pm.changeName(person, "Jane Doe")
 	pm.changeAge(person, 25)
@@ -84,6 +104,7 @@ fun main() {
 }
 
 // Output:
+// Name: Jane Doe, Age: 25, Height: 5.5, Weight: 150.0
 // Name: Jane Doe, Age: 25, Height: 5.5, Weight: 150.0
 // Name: Jane Doe, Age: 25, Height: 5.5, Weight: 150.0
 // Name: Jane Doe, Age: 25, Height: 5.5, Weight: 150.0
